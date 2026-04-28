469a194552272788da806e7431931b907ae9e3dd
Moi <2541482@cegeplimoilou.ca>
2026-04-28 12:58:40 -0400

    Création Etoile

--- src/Etoile.java
+++ src/Etoile.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Etoile {
+    int tempCouleur;
+    ArrayList<String> compoSurface;
+    Type classification;
+
+
+    public Etoile(int tempCouleur, ArrayList<String> compoSurface, Type classification) {
+        tempCouleur = this.tempCouleur;
+        compoSurface = this.compoSurface;
+        classification = this.classification;
+    }
+
+    public int getTempCouleur() {
+        return tempCouleur;
+    }
+
+    public Type getClassification() {
+        return classification;
+    }
+
 }
+
+
