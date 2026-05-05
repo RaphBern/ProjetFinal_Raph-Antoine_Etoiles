b249aeb0741334c82389bb12745ca4cb95e63226
Raph <2541482@cegeplimoilou.ca>
2026-05-05 12:02:28 -0400

    Ajout composition

---
+++ src/Étoile/CompoRaies.java
@@ -0,0 +1,5 @@
+package Étoile;
+
+public enum CompoRaies {
+
+}
--- src/Étoile/Composition.java
+++ src/Étoile/Composition.java
@@ -1,5 +1,10 @@
 package Étoile;
 
+import java.util.ArrayList;
+
 public class Composition {
+    public Composition()    {
+        ArrayList<CompoRaies> elements = new ArrayList<>();
+    }
 
 }
