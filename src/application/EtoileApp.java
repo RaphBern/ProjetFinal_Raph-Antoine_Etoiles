f4e7ed958ac923ee06a7e2fb12722317d9e9ef86
antoineTremblay1007 <2540948@cegeplimoilou.ca>
2026-05-05 13:11:56 -0400

    Création scanner début

--- src/application/EtoileApp.java
+++ src/application/EtoileApp.java
@@ -5,6 +5,7 @@ import Étoile.Etoile;
 import Étoile.RegleTemp;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class EtoileApp {
     public static void main(String[] args) throws IOException {
@@ -12,5 +13,18 @@ public class EtoileApp {
         Etoile etoile1 = new Etoile(10001,new Composition(), "Bleue");
         RegleTemp regleTemp = new RegleTemp();
         System.out.println(etoile1.getType());
+
+
+        Scanner lecteur = new Scanner(System.in);
+        System.out.println("Entrer la température de l'étoile (si la donnée est inconnu écrire R):");
+        String entreeTemp = lecteur.nextLine();
+        Integer.parseInt(entreeTemp);
+
+        System.out.println("Entrer la couleur de l'étoile (si la couleur est inconnu écrire A):");
+        String entreeColor = lecteur.nextLine();
+
+        System.out.println("Entrer un element de la composition du raie d'absorbtion ()");
+        String entreeElements = lecteur.nextLine();
+
     }
 }
