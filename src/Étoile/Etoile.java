d62f0f20800d38971800085bc931e92097cbbd0f
Raph <2541482@cegeplimoilou.ca>
2026-05-12 12:07:41 -0400

    Avancée getType

--- src/Étoile/Etoile.java
+++ src/Étoile/Etoile.java
@@ -3,6 +3,7 @@ package Étoile;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.annotation.ElementType;
 import java.util.ArrayList;
 
 public class Etoile implements ClassificationEtoile {
@@ -55,6 +56,15 @@ public class Etoile implements ClassificationEtoile {
        }
     }
 
+    public ArrayList<CompoRaies> getCompoSurface() {
+        return compoSurface;
+    }
+
+    @Override
+    public String toString(){
+        return "Température de couleur: " + getTempCouleur() + "\n Couleur: " + getCouleur() + "\n Composition des raies: " + getCompoSurface();
+    }
+
     @Override
     public boolean associerEtoiles(Etoile etoile) {
         return false;
@@ -66,7 +76,8 @@ public class Etoile implements ClassificationEtoile {
         String Couleur;
         String Couleur2;
         Type typeTemp = null;
-        Type typeCouleur = null;
+        Type[] typeCouleur = null;
+
         String line;
         BufferedReader fichier = new BufferedReader(new FileReader("src/DonneesClassification/ClassificationsTemperature.csv"));
         fichier.readLine();
@@ -84,14 +95,30 @@ public class Etoile implements ClassificationEtoile {
         BufferedReader fichier1 = new BufferedReader(new FileReader("src/DonneesClassification/CLassificationCouleur.csv"));
         fichier1.readLine();
         line1 = fichier1.readLine();
+        int i = 0;
         while (line1 != null) {
             String[] etoilex = line1.split(",");
             Couleur = etoilex[0];
             Couleur2 = etoilex[1];
-            if(getCouleur().equals(Couleur) || getCouleur().equals(Couleur2));{
-                typeCouleur = Type.valueOf(etoilex[2]);
+
+            if(getCouleur().toString().equals(Couleur) || getCouleur().toString().equals(Couleur2)){
+               typeCouleur[i] = Type.valueOf(etoilex[2]);
+               i++;
             }
             line1 = fichier1.readLine();
+        }
+        String line2;
+        BufferedReader fichier2 = new BufferedReader(new FileReader("src/DonneesClassification/ClassificationsComposition.csv"));
+        fichier2.readLine();
+        line2 = fichier2.readLine();
+        while (line != null) {
+            String[] etoilez = line2.split(",");
+            tempMin = Integer.parseInt(etoilez[0]);
+            tempMax = Integer.parseInt(etoilez[1]);
+            if (getTempCouleur() > tempMin && getTempCouleur() < tempMax){
+                typeTemp = Type.valueOf(etoilez[2]);
+            }
+            line2 = fichier.readLine();
         }
         fichier1.close();
         fichier.close();
