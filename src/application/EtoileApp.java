2131598603e4fa5f782afa6a807b678493e15c20
Antoine <2540948@cegeplimoilou.ca>
2026-05-12 12:06:48 -0400

    Modification App et recommence classe RegleTemp

--- src/application/EtoileApp.java
+++ src/application/EtoileApp.java
@@ -4,6 +4,7 @@ import Étoile.CompoRaies;
 import Étoile.Couleurs;
 import Étoile.Etoile;
 import Étoile.RegleTemp;
+import Étoile.Type;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,53 +15,67 @@ public class EtoileApp {
 
         Scanner lecteur = new Scanner(System.in);
         Etoile etoile = new Etoile(0,null,null);
-        System.out.println("Entrer la température de l'étoile (si la donnée est inconnu écrire R):");
-        String entreeTemp = lecteur.nextLine();
-         while (!entreeTemp.equalsIgnoreCase("R")){
-            try {
-                int temp = Integer.parseInt(entreeTemp);
-                etoile.setTempCouleur(temp);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("Température impossible ou ");
-                entreeTemp = lecteur.nextLine();
+        System.out.println("Si vous vouler connaître le type d'une étoile appuiée sur R:\nSi vous vouler connaître les caractéristique d'un type entrer la lettre du type:");
+        String entree = lecteur.nextLine();
+            if(entree.equalsIgnoreCase("R")) {
+                System.out.println("Entrer la température de l'étoile (si la donnée est inconnu écrire R):");
+                String entreeTemp = lecteur.nextLine();
+                while (!entreeTemp.equalsIgnoreCase("R")) {
+                    try {
+                        int temp = Integer.parseInt(entreeTemp);
+                        etoile.setTempCouleur(temp);
+                        break;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Température impossible ou ");
+                        entreeTemp = lecteur.nextLine();
+                    }
+
+                }
+
+                System.out.println("Entrer la couleur de l'étoile (si la couleur est inconnue écrire R):");
+                String entreeColor = lecteur.nextLine();
+                entreeColor = entreeColor.toUpperCase();
+                while (!entreeColor.equals("R")) {
+                    try {
+                        Couleurs couleur = Couleurs.valueOf(entreeColor);
+                        etoile.setCouleur(couleur);
+                        break;
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Couleur invalide ou inutile pour la classification");
+                        System.out.println("Entrez à nouveau la couleur ou R si vous n'avez pas d'autre données");
+
+                    }
+                    entreeColor = lecteur.nextLine();
+                }
+
+                System.out.println("Entrer un element de la composition du raie d'absorbtion (si inconnu ecrire R)");
+                String entreeElements = lecteur.nextLine();
+                ArrayList<CompoRaies> compo = new ArrayList<>();
+                while (!entreeElements.equalsIgnoreCase("R")) {
+                    entreeElements = entreeElements.toUpperCase();
+                    try {
+                        compo.add(CompoRaies.valueOf(entreeElements));
+                        System.out.println("Entrée valide, entrer le prochain élément (si le reste est inconnu, entrer R)");
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Élément inconnu ou inutile pour la classification");
+                        System.out.println("Entrez à nouveau l'élément ou R si le reste de la composition des raies est inconnue ");
+                    }
+                    entreeElements = lecteur.nextLine();
+                }
+                etoile.setCompoSurface(compo);
+                System.out.println(compo);
+                System.out.println(etoile.toString());
             }
-
-        }
-
-        System.out.println("Entrer la couleur de l'étoile (si la couleur est inconnue écrire R):");
-        String entreeColor = lecteur.nextLine();
-        entreeColor = entreeColor.toUpperCase();
-      while (!entreeColor.equals("R")) {
-          try {
-              Couleurs couleur = Couleurs.valueOf(entreeColor);
-              etoile.setCouleur(couleur);
-             break;
-          } catch (IllegalArgumentException e) {
-              System.out.println("Couleur invalide ou inutile pour la classification");
-              System.out.println("Entrez à nouveau la couleur ou R si vous n'avez pas d'autre données");
-
-          }
-          entreeColor = lecteur.nextLine();
-      }
-
-        System.out.println("Entrer un element de la composition du raie d'absorbtion (si inconnu ecrire R)");
-        String entreeElements = lecteur.nextLine();
-         ArrayList<CompoRaies> compo = new ArrayList<>();
-         while (!entreeElements.equalsIgnoreCase("R")) {
-             entreeElements = entreeElements.toUpperCase();
-              try {
-                  compo.add(CompoRaies.valueOf(entreeElements));
-                  System.out.println("Entrée valide, entrer le prochain élément (si le reste est inconnu, entrer R)");
-              }catch (IllegalArgumentException e) {
-                  System.out.println("Élément inconnu ou inutile pour la classification");
-                  System.out.println("Entrez à nouveau l'élément ou R si le reste de la composition des raies est inconnue ");
-              }
-             entreeElements = lecteur.nextLine();
-         }
-         etoile.setCompoSurface(compo);
-        System.out.println(compo);
-        System.out.println(etoile.toString());
+            else{
+                while(!entree.equalsIgnoreCase("R")) {
+                try {
+                    Type type = Type.valueOf(entree);
+                    System.out.println(RegleTemp(entree));
+                }catch(IllegalArgumentException e) {
+                    System.out.println("Type inconnu ou il n'a pas été écrit en majuscule");
+                }
+                entree = lecteur.nextLine();}
+            }
     }
 
 }
--- src/Étoile/RegleTemp.java
+++ src/Étoile/RegleTemp.java
@@ -4,30 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class RegleTemp implements ClassificationEtoile {
-    @Override
-    public boolean associerEtoiles(Etoile etoile) {
-        return false;
+public class RegleTemp {
+
+    public RegleTemp(Type type) {
+
     }
-@Override
-    public Type getType() throws IOException {
-//        int tempMin;
-//        int tempMax;
-//        Type type = null;
-//        String line;
-//        BufferedReader fichier = new BufferedReader(new FileReader("src/ClassificationsTemperature.csv"));
-//        fichier.readLine();
-//        line = fichier.readLine();
-//        while (line != null) {
-//            String[] etoilez = line.split(",");
-//            tempMin = Integer.parseInt(etoilez[0]);
-//            tempMax = Integer.parseInt(etoilez[1]);
-//            if (getTempCouleur() > tempMin && getTempCouleur() < tempMax){
-//                type = Type.valueOf(etoilez[2]);
-//            }
-//            line = fichier.readLine();
-//        }
-// fichier.close();
-        return null;
-    }
+
 }
